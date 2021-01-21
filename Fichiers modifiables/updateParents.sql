329e6bbc5303f95a95189518d191a63ddad2f6e3
Paola Costa <paola.costa98@gmail.com>
2021-01-21 19:18:55 +0100

    update des insert et update

---
+++ Fichiers modifiables/updateParents.sql
@@ -0,0 +1,100 @@
+Update animals values (mother_id, father_id) set ( null, null ) Where id=z;
+
+
+Update animals values (mother_id, father_id) set ( 3, null ) Where id=20;
+Update animals values (mother_id, father_id) set ( 85, 18 ) Where id=35;
+Update animals values (mother_id, father_id) set ( 8, 1 ) Where id=41;
+Update animals values (mother_id, father_id) set ( 26, 100 ) Where id=50;
+Update animals values (mother_id, father_id) set ( 28, null ) Where id=57;
+Update animals values (mother_id, father_id) set ( 14, 12 ) Where id=61;
+Update animals values (mother_id, father_id) set ( 42, null ) Where id=62;
+Update animals values (mother_id, father_id) set ( 32, 14 ) Where id=64;
+Update animals values (mother_id, father_id) set ( 48, null ) Where id=66;
+Update animals values (mother_id, father_id) set ( 3, 20 ) Where id=68;
+Update animals values (mother_id, father_id) set ( 3, 20 ) Where id=70;
+Update animals values (mother_id, father_id) set ( 57, null ) Where id=73;
+Update animals values (mother_id, father_id) set ( null, 45 ) Where id=74;
+Update animals values (mother_id, father_id) set ( 13, 24 ) Where id=77;
+Update animals values (mother_id, father_id) set ( 13, 24 ) Where id=78;
+Update animals values (mother_id, father_id) set ( 29, 25 ) Where id=81;
+Update animals values (mother_id, father_id) set ( null, 33 ) Where id=86;
+Update animals values (mother_id, father_id) set ( 10, null ) Where id=89;
+Update animals values (mother_id, father_id) set ( 53, 79 ) Where id=90;
+Update animals values (mother_id, father_id) set ( 11, 21 ) Where id=91;
+Update animals values (mother_id, father_id) set ( 10, null ) Where id=94;
+Update animals values (mother_id, father_id) set ( 48, null ) Where id=99;
+Update animals values (mother_id, father_id) set ( 43, null ) Where id=100;
+Update animals values (mother_id, father_id) set ( 40, 44 ) Where id=103;
+Update animals values (mother_id, father_id) set ( 89, null ) Where id=104;
+Update animals values (mother_id, father_id) set ( 75, 19 ) Where id=105;
+Update animals values (mother_id, father_id) set ( 71, 80 ) Where id=106;
+Update animals values (mother_id, father_id) set ( 51, null ) Where id=108;
+Update animals values (mother_id, father_id) set ( 32, 14 ) Where id=110;
+Update animals values (mother_id, father_id) set ( null, 77 ) Where id=119;
+Update animals values (mother_id, father_id) set ( 28, 73 ) Where id=120;
+Update animals values (mother_id, father_id) set ( 38, null ) Where id=121;
+Update animals values (mother_id, father_id) set ( 34, 25 ) Where id=122;
+Update animals values (mother_id, father_id) set ( 75, 19 ) Where id=124;
+Update animals values (mother_id, father_id) set ( 32, 14 ) Where id=125;
+Update animals values (mother_id, father_id) set ( 38, null ) Where id=129;
+Update animals values (mother_id, father_id) set ( 36, 5 ) Where id=130;
+Update animals values (mother_id, father_id) set ( null, 46 ) Where id=135;
+Update animals values (mother_id, father_id) set ( 14, 12 ) Where id=136;
+Update animals values (mother_id, father_id) set ( 115, 98 ) Where id=138;
+Update animals values (mother_id, father_id) set ( null, 97 ) Where id=144;
+Update animals values (mother_id, father_id) set ( 11, 21 ) Where id=145;
+Update animals values (mother_id, father_id) set ( 84, null ) Where id=146;
+Update animals values (mother_id, father_id) set ( null, 27 ) Where id=147;
+Update animals values (mother_id, father_id) set ( 40, 44 ) Where id=148;
+Update animals values (mother_id, father_id) set ( 75, 19 ) Where id=149;
+Update animals values (mother_id, father_id) set ( null, 27 ) Where id=151;
+Update animals values (mother_id, father_id) set ( 131, 101) Where id=152;
+Update animals values (mother_id, father_id) set ( 75, null ) Where id=155;
+Update animals values (mother_id, father_id) set ( 115, 98 ) Where id=156;
+Update animals values (mother_id, father_id) set ( 122, 81 ) Where id=157;
+Update animals values (mother_id, father_id) set ( 122, 81 ) Where id=158;
+Update animals values (mother_id, father_id) set ( 31, 37 ) Where id=160;
+Update animals values (mother_id, father_id) set ( null, 27 ) Where id=161;
+Update animals values (mother_id, father_id) set ( 59, 16 ) Where id=162;
+Update animals values (mother_id, father_id) set ( 43, 50 ) Where id=164;
+Update animals values (mother_id, father_id) set ( 95, 155 ) Where id=165;
+Update animals values (mother_id, father_id) set ( 62, 66 ) Where id=168;
+Update animals values (mother_id, father_id) set ( 71, 80 ) Where id=169;
+Update animals values (mother_id, father_id) set ( 85, 18) Where id=170;
+Update animals values (mother_id, father_id) set ( 95, 155) Where id=172;
+Update animals values (mother_id, father_id) set ( 59, 16) Where id=174;
+Update animals values (mother_id, father_id) set ( 32, 5 ) Where id=175;
+Update animals values (mother_id, father_id) set ( 172, null ) Where id=176;
+Update animals values (mother_id, father_id) set ( 53, 79) Where id=177;
+Update animals values (mother_id, father_id) set ( null, 27 ) Where id=179;
+Update animals values (mother_id, father_id) set ( 172, null ) Where id=180;
+Update animals values (mother_id, father_id) set ( 8, 1 ) Where id=181;
+Update animals values (mother_id, father_id) set ( 58, 198 ) Where id=183;
+Update animals values (mother_id, father_id) set ( 115, 98 ) Where id=184;
+Update animals values (mother_id, father_id) set ( 58, 198 ) Where id=185;
+Update animals values (mother_id, father_id) set ( 135, 67 ) Where id=188;
+Update animals values (mother_id, father_id) set ( 62, 66 ) Where id=189;
+Update animals values (mother_id, father_id) set ( 132, 114 ) Where id=190;
+Update animals values (mother_id, father_id) set ( 131, 101 ) Where id=191;
+Update animals values (mother_id, father_id) set ( 53, 79 ) Where id=193;
+Update animals values (mother_id, father_id) set ( 83, 129 ) Where id=194;
+Update animals values (mother_id, father_id) set ( 70, null ) Where id=195;
+Update animals values (mother_id, father_id) set ( 158, 140 ) Where id=196;
+Update animals values (mother_id, father_id) set ( 69, 198 ) Where id=197;
+Update animals values (mother_id, father_id) set ( 179, 27 ) Where id=198;
+Update animals values (mother_id, father_id) set ( 145, null ) Where id=200;
+Update animals values (mother_id, father_id) set ( 141, 150 ) Where id=203;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
